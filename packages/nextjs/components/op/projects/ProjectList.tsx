c30433abe000cb6939618c215fb65b97e427b5bb
KcPele <fidekg123@gmail.com>
2023-07-27 06:54:41 +0100

    added container queries

--- packages/nextjs/components/op/projects/ProjectList.tsx
+++ packages/nextjs/components/op/projects/ProjectList.tsx
@@ -10,20 +10,22 @@ interface IProjectData {
 const ProjectList = ({ projectData }: { projectData: IProjectData[] }) => {
   return (
     <div className=" border-[#a2aab6] border-2 rounded-3xl gap-10 grid  px-8 py-10">
-      <div className="grid sm:grid-flow-col justify-between">
-        <h3 className="text-2xl font-bold   items-center">
-          <span>6 projects</span> <span className="text-center rounded-full bg-black w-2 h-2"></span>{" "}
-          <span>100k OP allocated</span>
-        </h3>
-        <div className="grid grid-flow-col gap-6">
-          <button className="btn rounded-lg border-[#d3dde7] btn-outline text-[#4d4f52]">
-            <AdjustmentsHorizontalIcon className="w-5 h-5" />
-            <span className="pl-2">Edit distribution</span>
-          </button>
-          <button className="btn bg-[#ff0000] rounded-lg border-[#ff0000] btn-outline text-[#ffffff]">
-            <SquaresPlusIcon className="w-5 h-5" />
-            <span className="pl-2">Add to ballot</span>
-          </button>
+      <div className="project__header-container">
+        <div className="project__header-container--content">
+          <h3 className="text-2xl font-bold   items-center">
+            <span>6 projects</span> <span className="text-center rounded-full bg-black w-2 h-2"></span>{" "}
+            <span>100k OP allocated</span>
+          </h3>
+          <div className="grid grid-flow-col gap-3 sm:gap-6">
+            <button className=" grid grid-flow-col items-center p-2  justify-center rounded-lg border-[#d3dde7] border-2 text-[#4d4f52]">
+              <AdjustmentsHorizontalIcon className="w-5 h-5" />
+              <span className="pl-2">Edit distribution</span>
+            </button>
+            <button className="grid grid-flow-col items-center justify-center p-2  bg-[#ff0000] rounded-lg border-[#ff0000]  text-[#ffffff]">
+              <SquaresPlusIcon className="w-5 h-5" />
+              <span className="pl-2">Add to ballot</span>
+            </button>
+          </div>
         </div>
       </div>
       <div>
