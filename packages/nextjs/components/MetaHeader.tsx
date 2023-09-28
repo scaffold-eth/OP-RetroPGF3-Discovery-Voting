ecd48e325e6ab4123d787f2d9af5c1e71dd48584
electrone901 <Carbajalluis@rocketmail.com>
2023-09-28 18:08:41 -0400

    add favicon

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -45,7 +45,7 @@ export const MetaHeader = ({
         </>
       )}
       {twitterCard && <meta name="twitter:card" content={twitterCard} />}
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
       {children}
     </Head>
   );
